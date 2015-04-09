84f3d22764ca0412efde7f4f3c4fefee6f19d9d6
i474232898 <i474232898@gmail.com>
2015-04-09 19:51:22 +0300

    Added methods to Author

--- src/main/java/com/soft/library/ui/Commands/AuthorCommands/DeleteAuthorCommand.java
+++ src/main/java/com/soft/library/ui/Commands/AuthorCommands/DeleteAuthorCommand.java
@@ -1,5 +1,8 @@
 package com.soft.library.ui.Commands.AuthorCommands;
 
+import java.util.Scanner;
+
+import com.soft.library.data.Author;
 import com.soft.library.ui.CommandCore.ICommand;
 
 /**
@@ -8,7 +11,9 @@ import com.soft.library.ui.CommandCore.ICommand;
 public class DeleteAuthorCommand implements ICommand {
     @Override
     public void execute() {
-
+        Scanner scanner = new Scanner(System.in);
+        Author.removeById(scanner.next());
+        scanner.close();
     }
 
     @Override
