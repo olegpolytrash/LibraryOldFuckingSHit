c2b4e45f72ed5ea899c79b73bf7b4e151cccf3da
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-04-10 00:33:07 +0300

    initial commit

--- src/main/java/com/soft/library/ui/Commands/PublisherCommands/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * All classes in this package represent a command that can be executed in the database for the Publisher table.
- * These classes are parts of the command pattern.
- */
-package com.soft.library.ui.Commands.PublisherCommands;
--- src/main/java/com/soft/library/ui/Menus/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * All classes in this package represent menu options, that will show a new menu if selected.
- * These classes are parts of the command pattern.
- */
-package com.soft.library.ui.Menus;
