6fe0e31a48f47e0fe77305441238fd7c696dbc0c
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-04-10 00:44:58 +0300

    initial commit

---
+++ src/main/java/com/soft/library/ui/Menus/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * All classes in this package represent menu options, that will show a new menu if selected.
+ * These classes are parts of the command pattern.
+ */
+package com.soft.library.ui.Menus;
