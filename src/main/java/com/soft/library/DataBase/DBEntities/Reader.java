2f64aa097ffd79f7742220183168aab2f1d74788
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-04-10 19:34:19 +0300

    initial commit

---
+++ src/main/java/com/soft/library/DataBase/DBEntities/Reader.java
@@ -0,0 +1,66 @@
+package com.soft.library.DataBase.DBEntities;
+
+import java.sql.Date;
+
+/**
+ * Database entry.
+ */
+public class Reader {
+    private int id;
+    private String name;
+    private String surname;
+    private String mobilePhone;
+    private String address;
+    private Date birthDate;
+
+    public Reader() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
