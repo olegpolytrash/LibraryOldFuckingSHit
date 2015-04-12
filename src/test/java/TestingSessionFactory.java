5fcc1774c8f861a322a83e04445eb6f3ac2c5292
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-04-12 04:33:34 +0300

    added config files for testing

---
+++ src/test/java/TestingSessionFactory.java
@@ -0,0 +1,20 @@
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Singleton for the TestingSessionFactory object.
+ * Usage example: TestingSessionFactory.INSTANCE.get().openSession();
+ */
+public enum TestingSessionFactory {
+    INSTANCE;
+
+    private final org.hibernate.SessionFactory sessionFactory;
+
+    TestingSessionFactory() {
+        sessionFactory = new Configuration().configure("hibernateTest.cfg.xml").buildSessionFactory();
+    }
+
+    public org.hibernate.SessionFactory get()
+    {
+        return sessionFactory;
+    }
+}
