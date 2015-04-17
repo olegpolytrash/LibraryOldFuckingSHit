60c5f681321f9d60eefbd8e1f26bcdb967adfe64
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-04-18 00:13:43 +0300

    asdads

--- src/main/java/com/soft/library/DataBase/service/AdvBookService.java
+++ src/main/java/com/soft/library/DataBase/service/AdvBookService.java
@@ -1,52 +1,49 @@
 package com.soft.library.dataBase.service;
 
-import com.soft.library.dataBase.model.Book;
-import com.soft.library.dataBase.dao.BookDAO;
-import com.soft.library.dataBase.dao.impl.DaoFactory;
 
 public class AdvBookService {
-    private BookDAO bookDAO;
-
-    public AdvBookService() {
-        bookDAO = DaoFactory.getInstance().getBookDAOImpl();
-    }
-
-    public void addBook(String title) {
-        Book book = new Book();
-        book.setName(title);
-        bookDAO.save(book);
-    }
-
-    public int updateBooks(String oldName, String newName) {
-        int count = 0;
-        for (Book a : bookDAO.getAll()) {
-            if (a.getName().equals(oldName)) {
-                a.setName(newName);
-                bookDAO.update(a);
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public void printBooks() {
-        System.out.println("\nAll Books:");
-        for (Book a : bookDAO.getAll()) {
-            System.out.println("bookDAO: id=" + a.getId() + " Title="
-                    + a.getName());
-        }
-    }
-
-    public Book getBookById(int bookDAOId) {
-        return bookDAO.findById(bookDAOId);
-    }
-
-    public void deleteBook(String name) {
-        for (Book a : bookDAO.getAll()) {
-            if (a.getName().equalsIgnoreCase(name)) {
-                bookDAO.remove(a);
-                break;
-            }
-        }
-    }
+//    private BookDAO bookDAO;
+//
+//    public AdvBookService() {
+//        bookDAO = DaoFactory.getInstance().getBookDAOImpl();
+//    }
+//
+//    public void addBook(String title) {
+//        Book book = new Book();
+//        book.setName(title);
+//        bookDAO.save(book);
+//    }
+//
+//    public int updateBooks(String oldName, String newName) {
+//        int count = 0;
+//        for (Book a : bookDAO.getAll()) {
+//            if (a.getName().equals(oldName)) {
+//                a.setName(newName);
+//                bookDAO.update(a);
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//
+//    public void printBooks() {
+//        System.out.println("\nAll Books:");
+//        for (Book a : bookDAO.getAll()) {
+//            System.out.println("bookDAO: id=" + a.getId() + " Title="
+//                    + a.getName());
+//        }
+//    }
+//
+//    public Book getBookById(int bookDAOId) {
+//        return bookDAO.findById(bookDAOId);
+//    }
+//
+//    public void deleteBook(String name) {
+//        for (Book a : bookDAO.getAll()) {
+//            if (a.getName().equalsIgnoreCase(name)) {
+//                bookDAO.remove(a);
+//                break;
+//            }
+//        }
+//    }
 }
